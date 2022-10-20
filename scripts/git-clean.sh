975fd044a933b408ad24838c491c03457be12ffd
Stepan Anokhin <stepan.anokhin@gmail.com>
2022-10-20 19:41:34 +0700

    Update utility scripts

--- scripts/git-clean.sh
+++ scripts/git-clean.sh
@@ -5,6 +5,3 @@ git branch -D fix-1
 git branch -D fix-2
 git branch -D staging
 git push origin --delete staging
-git checkout -b staging
-git push origin staging
-git checkout master
---
+++ scripts/git-make-staging.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git checkout -b staging
+git push origin staging
+git checkout master
