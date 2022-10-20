ef2e877fe98349d46c1c96eae56ba6c28e195aef
Stepan Anokhin <stepan.anokhin@gmail.com>
2022-10-20 22:43:56 +0700

    Fix canary release

--- scripts/canary-release.sh
+++ scripts/canary-release.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Create branch-specific version
-npx lerna version --no-changelog --conventional-commits --conventional-prerelease --no-push --preid "$(git rev-parse --abbrev-ref HEAD)+$(git rev-parse --short HEAD)" --yes
+npx lerna version --no-changelog --conventional-commits --conventional-prerelease --no-push --preid "$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)" --yes
 
 # Publish canary release
-npx lerna publish from-git --dist-tag "$(git rev-parse --abbrev-ref HEAD)"
+npx lerna publish from-git --dist-tag "$(git rev-parse --abbrev-ref HEAD)" --yes
